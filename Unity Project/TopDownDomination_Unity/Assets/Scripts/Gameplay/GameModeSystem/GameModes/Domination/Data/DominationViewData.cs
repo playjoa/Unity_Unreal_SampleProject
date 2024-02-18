80aa7d1a9817d5a0409fb854d3acefb1809f7fb9
Joao Milone <joaocmil@hotmail.com>
2024-02-17 21:48:18 -0300

    Added Domination View UI Logic

---
+++ Unity Project/TopDownDomination_Unity/Assets/Scripts/Gameplay/GameModeSystem/GameModes/Domination/Data/DominationViewData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Gameplay.GameModeSystem.GameModes.Domination.Data
+{
+    [CreateAssetMenu(menuName = "GameSystems/GameModes/UI/DominationViewData", fileName = "New DominationViewData")]
+    public class DominationViewData : ScriptableObject
+    {
+        [Header("Colors")]
+        public Color DominatedBaseColor = Color.red;
+        public Color OwnedByPlayerColor = Color.blue;
+    }
+}
--- Unity Project/TopDownDomination_Unity/Assets/Scripts/Gameplay/GameModeSystem/GameModes/Domination/UI/DominationViewUI.cs
+++ Unity Project/TopDownDomination_Unity/Assets/Scripts/Gameplay/GameModeSystem/GameModes/Domination/UI/DominationViewUI.cs
@@ -1,17 +1,72 @@
-﻿using Gameplay.GameModeSystem.GameModes.Domination.Components;
+﻿using Gameplay.Entity.Base.Data;
+using Gameplay.GameModeSystem.GameModes.Domination.Components;
+using Gameplay.GameModeSystem.GameModes.Domination.Data;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Gameplay.GameModeSystem.GameModes.Domination.UI
 {
     public class DominationViewUI : MonoBehaviour
     {
+        [Header("Data")] 
+        [SerializeField] private DominationViewData dominationViewData;
+        
         [Header("Texts")] 
         [SerializeField] private TextMeshProUGUI dominationNameTMP;
+
+        [Header("Images")] 
+        [SerializeField] private Image dominationBackGroundImage;
+        [SerializeField] private Image dominationFillImage;
+
+        private DominationZone _displayingZone;
         
         public void Initiate(DominationZone dominationZone)
         {
+            _displayingZone = dominationZone;
             dominationNameTMP.text = dominationZone.DominationZoneName;
+            
+            SetColors(dominationZone);
+            SetCaptureFillAmount(0f);
+            
+            _displayingZone.OnZoneCaptureInProgress += OnZoneCaptureInProgressHandler;
+            _displayingZone.OnZoneCaptured += OnZoneCapturedHandle;
+        }
+
+        private void OnDestroy()
+        {
+            _displayingZone.OnZoneCaptureInProgress -= OnZoneCaptureInProgressHandler;
+            _displayingZone.OnZoneCaptured -= OnZoneCapturedHandle;
+        }
+        
+        private void SetColors(DominationZone dominationZone)
+        {
+            if (dominationZone.EntityOwnerType == EntityType.Player)
+            {
+                dominationBackGroundImage.color = dominationViewData.OwnedByPlayerColor;
+                dominationFillImage.color = dominationViewData.DominatedBaseColor;
+            }
+            else
+            {
+                dominationBackGroundImage.color = dominationViewData.DominatedBaseColor;
+                dominationFillImage.color = dominationViewData.OwnedByPlayerColor;
+            }
+        }
+
+        private void SetCaptureFillAmount(float fillAmount)
+        {
+            dominationFillImage.fillAmount = fillAmount;
+        }
+        
+        private void OnZoneCaptureInProgressHandler(DominationZone dominationZone)
+        {
+            SetCaptureFillAmount(dominationZone.CaptureProgress);
+        }
+
+        private void OnZoneCapturedHandle(DominationZone dominationZone)
+        {
+            SetColors(dominationZone);
+            SetCaptureFillAmount(0f);
         }
     }
 }
