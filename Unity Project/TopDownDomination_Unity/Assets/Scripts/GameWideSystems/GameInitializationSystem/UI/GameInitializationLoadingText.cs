732c4e327a70ade185981ae59a3b32d839cada46
Joao Milone <joaocmil@hotmail.com>
2024-02-21 10:24:59 -0300

    Fix Lighting in TestMap

---
+++ Unity Project/TopDownDomination_Unity/Assets/Scripts/GameWideSystems/GameInitializationSystem/UI/GameInitializationLoadingText.cs
@@ -0,0 +1,28 @@
+﻿using GameWideSystems.GameInitialization.Controller;
+using GameWideSystems.GameInitialization.Interfaces;
+using TMPro;
+using UnityEngine;
+
+namespace GameWideSystems.GameInitializationSystem.UI
+{
+    public class GameInitializationLoadingText : MonoBehaviour
+    {
+        [Header("Texts")]
+        [SerializeField] private TextMeshProUGUI loadingTMP;
+        
+        private void Awake()
+        {
+            GameInitializationController.OnGameSystemStartedLoading += OnStartedLoadingSystemHandler;
+        }
+
+        private void OnDestroy()
+        {
+            GameInitializationController.OnGameSystemStartedLoading -= OnStartedLoadingSystemHandler;
+        }
+        
+        private void OnStartedLoadingSystemHandler(IGameWideSystem loadingSystem)
+        {
+            loadingTMP.text = $"Loading {loadingSystem.AppSystemName}...";
+        }
+    }
+}
